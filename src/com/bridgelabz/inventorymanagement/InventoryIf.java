623d146a81ec9fb5efad4137c37434bc2703b394
Vinay288 <vinayhiremath288@gmail.com>
2021-09-06 00:27:05 +0530

    ADDED:defined method for printing value of manager and factory

--- src/com/bridgelabz/inventorymanagement/InventoryIf.java
+++ src/com/bridgelabz/inventorymanagement/InventoryIf.java
@@ -3,4 +3,6 @@ package com.bridgelabz.inventorymanagement;
 public interface InventoryIf {
 	void valueOf(Item itemArray[],String itemName);
 	void valueOf(Item itemArray[]);
+	void valueOf(InventoryManager manager);
+	void valueOf(InventoryFactory factory);
 }
