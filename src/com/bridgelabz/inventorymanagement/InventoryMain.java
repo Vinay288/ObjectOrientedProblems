40ec20f7dbd86385ba7bf015e6a6ae851744389d
Vinay288 <vinayhiremath288@gmail.com>
2021-09-06 00:28:53 +0530

    ADDED:function calls to methods by manager and factory objects

--- src/com/bridgelabz/inventorymanagement/InventoryMain.java
+++ src/com/bridgelabz/inventorymanagement/InventoryMain.java
@@ -2,26 +2,36 @@ package com.bridgelabz.inventorymanagement;
 
 import java.util.Scanner;
 
-import com.bridgelabz.stockmanagement.Stock;
-import com.bridgelabz.stockmanagement.StockPortfolio;
-
 public class InventoryMain {
 
 	public static void main(String[] args) {
 		System.out.println("Enter total number of items");
 		Scanner scanner = new Scanner(System.in);
 		int numberOfItems =scanner.nextInt();
-		Inventory inventory=new Inventory(numberOfItems);
+		InventoryFactory factory1=new InventoryFactory("factory1",numberOfItems);
+		InventoryFactory factory2=new InventoryFactory("factory2",numberOfItems);
+		InventoryManager manager=new InventoryManager(2);
+		
 		for(int i=0;i<numberOfItems;i++) {
 			System.out.println("enter item name,weight and price per kg");
 			String name=scanner.next();
 			float weight=scanner.nextFloat();
 			float pricePerKg=scanner.nextFloat();
 			Item item = new Item(name,weight,pricePerKg);
-			inventory.addItem(item);
+			factory1.addItem(item);
 		}
+		for(int i=0;i<numberOfItems;i++) {
+			System.out.println("enter item name,weight and price per kg");
+			String name=scanner.next();
+			float weight=scanner.nextFloat();
+			float pricePerKg=scanner.nextFloat();
+			Item item = new Item(name,weight,pricePerKg);
+			factory2.addItem(item);
+		}
+		manager.addFactory(factory1);
+		manager.addFactory(factory2);
 		InventoryImpl inventoryImpl=new InventoryImpl();
-		inventoryImpl.valueOf(inventory.getItemArray());
+		inventoryImpl.valueOf(manager);
 
 	}
 
