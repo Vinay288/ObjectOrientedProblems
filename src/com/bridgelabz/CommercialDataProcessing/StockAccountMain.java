9046657285a2c45df5fa7cebea017ee582b4571f
Vinay288 <vinayhiremath288@gmail.com>
2021-09-05 22:15:39 +0530

    ADDED: main class which creats stock object and calls operation methods

--- src/com/bridgelabz/CommercialDataProcessing/StockAccountMain.java
+++ src/com/bridgelabz/CommercialDataProcessing/StockAccountMain.java
@@ -7,8 +7,8 @@ public class StockAccountMain {
 	public static void main(String[] args) {
 		System.out.println("enter total number of company");
 		Scanner scanner = new Scanner(System.in);
-		int numberOfCompany=scanner.nextInt();
-		StockAccount stockAccount=new StockAccount(numberOfCompany);
+		int numberOfCompany = scanner.nextInt();
+		StockAccount stockAccount = new StockAccount(numberOfCompany);
 		stockAccount.buy(10, "idea");
 		stockAccount.sell(5, "idea");
 		stockAccount.printReport();
